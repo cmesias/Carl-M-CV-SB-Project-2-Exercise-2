374eb74af23605f46b82938fd20596f9aecf1af7
Carl <carlmesias20@gmail.com>
2024-06-30 18:50:39 -0700

    remove console log under answers

--- script.js
+++ script.js
@@ -14,18 +14,15 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 // Get first letter and convert it to lowercase
 let firstLetterLowerCase = friend[0].toLowerCase();
-console.log(`firstLetterLowerCase: ${firstLetterLowerCase}`);
 
 // Get letter that was lowercased and find the index of it from alphabet string
 let indexOfFirstLetter = alphabet.indexOf( firstLetterLowerCase );
-console.log(`indexOfFirstLetter: ${indexOfFirstLetter}`);
 
 /* Question 1 */
 // The reason the result is 1 instead of 2 is because we start with the 0 index, then 1, then so on
 
 /* Step 3 */
 let encryptedLetterShifted = alphabet[indexOfFirstLetter + shiftValue];
-console.log(`encryptedLetterShifted: ${encryptedLetterShifted}`);
 
 /* Question 2 */
 // using the % will help in getting a value that is always within the bounds of the alphabet such as:
@@ -36,13 +33,10 @@ let lengthOfAlphabet = alphabet.length;
 
 /* Step 5 */
 let encryptedLetterShiftedAndWrapsAround = alphabet[ (indexOfFirstLetter + shiftValue) % 26 ];
-console.log(`encryptedLetterShiftedAndWrapsAround: ${encryptedLetterShiftedAndWrapsAround}`);
 
 /* Step 6 */
 let encryptedMsg = "EUXWXV";
 let extractFirstThreeChars = encryptedMsg.slice(0, 3);
-
-console.log(`extractFirstThreeChars: ${extractFirstThreeChars}`);
 
 
 
